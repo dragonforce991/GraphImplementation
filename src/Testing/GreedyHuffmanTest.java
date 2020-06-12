fd8ceb1e9b0d97d39a259c71333149168bb49f78
MassBroken <massarotti.antonino@gmail.com>
2020-06-12 22:53:26 +0200

    Nuova classe di test per Huffman

---
+++ src/Testing/GreedyHuffmanTest.java
@@ -0,0 +1,37 @@
+package Testing;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import upo.greedy.Greedy;
+
+public class GreedyHuffmanTest {
+
+	@Test
+    public void HuffmanTest() {
+    	Character[] alphabet = new Character[6];
+    	int[] freq = new int[6];
+    	char start = 'a';
+    	for(int i=0; i<6; i++) {
+    		alphabet[i] = start++;
+    	}
+    	
+    	freq[0] = 45;
+    	freq[1] = 13;
+    	freq[2] = 12;
+    	freq[3] = 16;
+    	freq[4] = 9;
+    	freq[5] = 5;
+    	
+    	Map<Character,String> result = Greedy.getHuffmanCodes(alphabet, freq);
+    	Assert.assertEquals(result.get('a'), "0");
+    	Assert.assertEquals(result.get('b'), "101");
+    	Assert.assertEquals(result.get('c'), "100");
+    	Assert.assertEquals(result.get('d'), "111");
+    	Assert.assertEquals(result.get('e'), "1101");
+    	Assert.assertEquals(result.get('f'), "1100");
+    	
+    }
+}
